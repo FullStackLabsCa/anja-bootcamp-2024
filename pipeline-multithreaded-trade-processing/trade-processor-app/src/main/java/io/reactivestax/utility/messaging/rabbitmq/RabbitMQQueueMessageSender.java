a0d1a2639334e31ddeb026dfd3f3554968c6634e
Anant.Jain <anant.jain@fullstacklabs.digital>
2024-10-16 16:02:01 -0400

    fixed connection closed issue

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/utility/messaging/rabbitmq/RabbitMQQueueMessageSender.java
+++ pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/utility/messaging/rabbitmq/RabbitMQQueueMessageSender.java
@@ -3,14 +3,18 @@ package io.reactivestax.utility.messaging.rabbitmq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import io.reactivestax.repository.hibernate.HibernateTradePayloadRepository;
 import io.reactivestax.utility.ApplicationPropertiesUtils;
 import io.reactivestax.utility.messaging.QueueMessageSender;
+import io.reactivestax.utility.rabbitmq.QueueUtil;
+import org.hibernate.Session;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class RabbitMQQueueMessageSender implements QueueMessageSender {
     private static RabbitMQQueueMessageSender instance;
+//    private static Channel channel;
     private static final ThreadLocal<Channel> channelThreadLocal = new ThreadLocal<>();
 
     private RabbitMQQueueMessageSender() {
@@ -25,11 +29,11 @@ public class RabbitMQQueueMessageSender implements QueueMessageSender {
 
     @Override
     public Boolean sendMessageToQueue(String queueName, String message) throws IOException, TimeoutException {
-        try {
+        try{
             getRabbitMQChannel().basicPublish(ApplicationPropertiesUtils.getInstance().getQueueExchangeName(), queueName, null,
                     message.getBytes());
             return true;
-        } catch (Throwable e) {
+        }catch (Throwable e){
             e.printStackTrace();
             //TODO: Make custom exception here
             throw new RuntimeException(e);
@@ -38,22 +42,17 @@ public class RabbitMQQueueMessageSender implements QueueMessageSender {
 
     private static synchronized Channel getRabbitMQChannel() throws IOException, TimeoutException {
         if (channelThreadLocal.get() == null) {
-            Channel localChannel = getChannel();
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.setHost(ApplicationPropertiesUtils.getInstance().getQueueHost());
+            connectionFactory.setUsername(ApplicationPropertiesUtils.getInstance().getQueueUsername());
+            connectionFactory.setPassword(ApplicationPropertiesUtils.getInstance().getQueuePassword());
+            Connection connection = connectionFactory.newConnection();
+            Channel localChannel = connection.createChannel();
             localChannel.exchangeDeclare(ApplicationPropertiesUtils.getInstance().getQueueExchangeName(),
                     ApplicationPropertiesUtils.getInstance().getQueueExchangeType());
             channelThreadLocal.set(localChannel);
         }
         return channelThreadLocal.get();
-    }
-
-    private static Channel getChannel() throws IOException, TimeoutException {
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost(ApplicationPropertiesUtils.getInstance().getQueueHost());
-        connectionFactory.setUsername(ApplicationPropertiesUtils.getInstance().getQueueUsername());
-        connectionFactory.setPassword(ApplicationPropertiesUtils.getInstance().getQueuePassword());
-        try (Connection connection = connectionFactory.newConnection()) {
-            return connection.createChannel();
-        }
     }
 
 }
