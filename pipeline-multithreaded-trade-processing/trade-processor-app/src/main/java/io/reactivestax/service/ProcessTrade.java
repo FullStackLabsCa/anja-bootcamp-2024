c5b71562e7bf737acc08333aee076319315cdaa7
Anant.Jain <anant.jain@fullstacklabs.digital>
2024-12-05 14:18:34 -0500

    removed trading project

--- pipeline-multithreaded-trade-processing/trade-processor-app/src/main/java/io/reactivestax/service/ProcessTrade.java
+++
@@ -1,7 +0,0 @@
-package io.reactivestax.service;
-
-import java.sql.SQLException;
-
-public interface ProcessTrade {
-    void processTrade(String tradeId) throws SQLException, InterruptedException;
-}
