ce1753b6dc90ae73f5909701374bad74931511a8
Anant.Jain <anant.jain@fullstacklabs.digital>
2024-12-18 14:58:09 -0500

    hierarchy achieved

--- bny-melon/src/main/java/io/reactivestax/domainmodel/service/RuleSetReaderService.java
+++ bny-melon/src/main/java/io/reactivestax/domainmodel/service/RuleSetReaderService.java
@@ -17,55 +17,80 @@ public class RuleSetReaderService {
     public void readRulesAndProcess() {
         readRules().forEach(rule -> RuleNo.getEnumValue(rule.getRuleNo()).ifPresent(ruleNo -> {
             switch (ruleNo) {
-                case ONE: {
-                    RuleSet ruleSet = (RuleSet) rule;
-                    ruleSetList.add(ruleSet);
-                    ruleSetMap.put(ruleNo, ruleSet);
-                    break;
-                }
-                case TWO: {
-                    RuleSet ruleSet = (RuleSet) ruleSetMap.get(RuleNo.ONE);
-                    ruleSet.addAccount((Account) rule);
-                    break;
-                }
-                case THREE: {
-                    RuleSet ruleSet = (RuleSet) ruleSetMap.get(RuleNo.ONE);
-                    EligibilityGroup eligibilityGroup = (EligibilityGroup) rule;
-                    ruleSet.addEligibilityGroup(eligibilityGroup);
-                    ruleSetMap.put(RuleNo.THREE, eligibilityGroup);
-                    break;
-                }
-                case FOUR: {
-                    EligibilityGroup eligibilityGroup = (EligibilityGroup) ruleSetMap.get(RuleNo.THREE);
-                    EligibilityRuleDetails eligibilityRuleDetails = (EligibilityRuleDetails) rule;
-                    eligibilityGroup.addEligibilityRuleDetails(eligibilityRuleDetails);
-                    ruleSetMap.put(RuleNo.THREE, eligibilityRuleDetails);
-                    break;
-                }
-                case FIVE: {
-                    EligibilityRuleDetails eligibilityRuleDetails =
-                            (EligibilityRuleDetails) ruleSetMap.get(RuleNo.FOUR);
-                    eligibilityRuleDetails.addSetValues((SetValues) rule);
-                    break;
-                }
-                case SIX: {
-                    EligibilityRuleDetails eligibilityRuleDetails =
-                            (EligibilityRuleDetails) ruleSetMap.get(RuleNo.FOUR);
-                    eligibilityRuleDetails.addConcentrationLimit((ConcentrationLimit) rule);
-                    break;
-                }
-                case SEVEN: {
-                    EligibilityGroup eligibilityGroup = (EligibilityGroup) ruleSetMap.get(RuleNo.THREE);
-                    eligibilityGroup.addMarginCut((MarginCut) rule);
-                    break;
-                }
-                default:
+                case ONE -> prepareRuleSet(rule, ruleNo);
+                case TWO -> prepareAccount(rule);
+                case THREE -> prepareEligibilityGroup(rule);
+                case FOUR -> prepareEligibilityGroupDetails(rule);
+                case FIVE -> prepareSetValues(rule);
+                case SIX -> prepareConcentrationLimit(rule);
+                case SEVEN -> prepareMarginCut(rule);
             }
         }));
         ruleSetMap.clear();
+        printRuleSet();
     }
 
-    private void printRuleSet(){
+    private void prepareMarginCut(Rule rule) {
+        EligibilityGroup eligibilityGroup = (EligibilityGroup) ruleSetMap.get(RuleNo.THREE);
+        MarginCut marginCut = new MarginCut();
+        marginCut.setRuleNo(rule.getRuleNo());
+        marginCut.setRuleValue(rule.getRuleValue());
+        eligibilityGroup.addMarginCut(marginCut);
+    }
+
+    private void prepareConcentrationLimit(Rule rule) {
+        EligibilityRuleDetails eligibilityRuleDetails =
+                (EligibilityRuleDetails) ruleSetMap.get(RuleNo.FOUR);
+        ConcentrationLimit concentrationLimit = new ConcentrationLimit();
+        concentrationLimit.setRuleNo(rule.getRuleNo());
+        concentrationLimit.setRuleValue(rule.getRuleValue());
+        eligibilityRuleDetails.addConcentrationLimit(concentrationLimit);
+    }
+
+    private void prepareSetValues(Rule rule) {
+        EligibilityRuleDetails eligibilityRuleDetails =
+                (EligibilityRuleDetails) ruleSetMap.get(RuleNo.FOUR);
+        SetValues setValues = new SetValues();
+        setValues.setRuleNo(rule.getRuleNo());
+        setValues.setRuleValue(rule.getRuleValue());
+        eligibilityRuleDetails.addSetValues(setValues);
+    }
+
+    private void prepareEligibilityGroupDetails(Rule rule) {
+        EligibilityGroup eligibilityGroup = (EligibilityGroup) ruleSetMap.get(RuleNo.THREE);
+        EligibilityRuleDetails eligibilityRuleDetails = new EligibilityRuleDetails();
+        eligibilityRuleDetails.setRuleNo(rule.getRuleNo());
+        eligibilityRuleDetails.setRuleValue(rule.getRuleValue());
+        eligibilityGroup.addEligibilityRuleDetails(eligibilityRuleDetails);
+        ruleSetMap.put(RuleNo.FOUR, eligibilityRuleDetails);
+    }
+
+    private void prepareEligibilityGroup(Rule rule) {
+        RuleSet ruleSet = (RuleSet) ruleSetMap.get(RuleNo.ONE);
+        EligibilityGroup eligibilityGroup = new EligibilityGroup();
+        eligibilityGroup.setRuleNo(rule.getRuleNo());
+        eligibilityGroup.setRuleValue(rule.getRuleValue());
+        ruleSet.addEligibilityGroup(eligibilityGroup);
+        ruleSetMap.put(RuleNo.THREE, eligibilityGroup);
+    }
+
+    private void prepareAccount(Rule rule) {
+        RuleSet ruleSet = (RuleSet) ruleSetMap.get(RuleNo.ONE);
+        Account account = new Account();
+        account.setRuleNo(rule.getRuleNo());
+        account.setRuleValue(rule.getRuleValue());
+        ruleSet.addAccount(account);
+    }
+
+    private void prepareRuleSet(Rule rule, RuleNo ruleNo) {
+        RuleSet ruleSet = new RuleSet();
+        ruleSet.setRuleNo(rule.getRuleNo());
+        ruleSet.setRuleValue(rule.getRuleValue());
+        ruleSetList.add(ruleSet);
+        ruleSetMap.put(ruleNo, ruleSet);
+    }
+
+    private void printRuleSet() {
         ruleSetList.forEach(System.out::println);
     }
 
