a78c9b5e0de1a50d32760302dd16bc930b77fb87
Anant.Jain <anant.jain@fullstacklabs.digital>
2025-01-22 06:27:30 -0500

    added message processor app

---
+++ ems-message-processor-app/src/main/java/io/reactivestax/ems_message_processor_app/EmsMessageProcessorAppApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.ems_message_processor_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsMessageProcessorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsMessageProcessorAppApplication.class, args);
+	}
+
+}
---
+++ ems-message-processor-app/src/test/java/io/reactivestax/ems_message_processor_app/EmsMessageProcessorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reactivestax.ems_message_processor_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsMessageProcessorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
