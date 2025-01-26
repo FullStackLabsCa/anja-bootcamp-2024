cc11a40883add42e0e29d9d34a49749e38f45ea6
Anant.Jain <anant.jain@fullstacklabs.digital>
2025-01-26 04:37:42 -0500

    added tests for customer repository

--- ems-app/src/test/java/io/reactivestax/ems/repository/CustomerRepositoryTest.java
+++ ems-app/src/test/java/io/reactivestax/ems/repository/CustomerRepositoryTest.java
@@ -1,8 +1,16 @@
 package io.reactivestax.ems.repository;
 
+import io.reactivestax.ems.domain.Customer;
+import io.reactivestax.ems.enums.OtpLock;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 class CustomerRepositoryTest {
@@ -11,7 +19,54 @@ class CustomerRepositoryTest {
     private CustomerRepository customerRepository;
 
     @Test
+    void testSave(){
+        Customer customerEntity = getCustomerEntity();
+        Customer customer = customerRepository.save(customerEntity);
+        assertThat(customer).isNotNull();
+        assertThat(customer).isInstanceOf(Customer.class);
+        assertThat(customer.getCustomerId()).isNotNull();
+        assertThat(customer.getFirstName()).isEqualTo(customerEntity.getFirstName());
+        assertThat(customer.getLastName()).isEqualTo(customerEntity.getLastName());
+        assertThat(customer.getCreatedAt()).isEqualTo(customerEntity.getCreatedAt());
+        assertThat(customer.getUpdatedAt()).isEqualTo(customerEntity.getUpdatedAt());
+        assertThat(customer.getOtpLock()).isEqualTo(customerEntity.getOtpLock());
+    }
+
+    @Test
     void testFindById() {
-//        customerRepository.save()
+        Customer customer = customerRepository.save(getCustomerEntity());
+        Optional<Customer> optionalCustomer = customerRepository.findById(customer.getCustomerId());
+        optionalCustomer.ifPresent(customer1 -> {
+            assertThat(customer1).isNotNull();
+            assertThat(customer1).isInstanceOf(Customer.class);
+            assertThat(customer1.getCustomerId()).isNotNull();
+            assertThat(customer1.getFirstName()).isEqualTo(customer.getFirstName());
+            assertThat(customer1.getLastName()).isEqualTo(customer.getLastName());
+            assertThat(customer1.getCreatedAt()).isEqualTo(customer.getCreatedAt());
+            assertThat(customer1.getUpdatedAt()).isEqualTo(customer.getUpdatedAt());
+            assertThat(customer1.getOtpLock()).isEqualTo(customer.getOtpLock());
+        });
+    }
+
+    @Test
+    void testFindAllWithOtpLock(){
+        Customer customerEntity = getCustomerEntity();
+        Customer customerEntity2 = getCustomerEntity();
+        Customer customerEntity3 = getCustomerEntity();
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customerEntity);
+        customerList.add(customerEntity2);
+        customerList.add(customerEntity3);
+        List<Customer> customers = customerRepository.saveAll(customerList);
+        List<Customer> allWithOtpLock = customerRepository.findAllWithOtpLock(OtpLock.NOT_LOCKED);
+        assertThat(customers).isEqualTo(allWithOtpLock);
+    }
+
+    private Customer getCustomerEntity() {
+        return Customer.builder()
+                .firstName("FirstName")
+                .lastName("LastName")
+                .otpLock(OtpLock.NOT_LOCKED)
+                .build();
     }
 }
