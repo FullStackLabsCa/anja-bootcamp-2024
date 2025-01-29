1a168bb85a6855aac70deb215f699552e123aa2b
Anant.Jain <anant.jain@fullstacklabs.digital>
2025-01-29 10:38:08 -0500

    added initial project for active life

---
+++ active-life-canada-app/src/main/java/io/reactivestax/active_life_canada_app/ActiveLifeCanadaAppApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.active_life_canada_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveLifeCanadaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveLifeCanadaAppApplication.class, args);
+	}
+
+}
---
+++ active-life-canada-app/src/test/java/io/reactivestax/active_life_canada_app/ActiveLifeCanadaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reactivestax.active_life_canada_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveLifeCanadaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
