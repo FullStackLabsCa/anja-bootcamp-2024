c5dc1638c37091248abe63a81b14c51646212382
Anant.Jain <anant.jain@fullstacklabs.digital>
2025-02-01 16:36:25 -0500

    added mappers and dtos

--- active-life-canada-app/src/main/java/io/reactivestax/active/life/canada/dto/AgeGroupDto.java
+++ active-life-canada-app/src/main/java/io/reactivestax/active/life/canada/dto/AgeGroupDto.java
@@ -1,5 +1,14 @@
 package io.reactivestax.active.life.canada.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AgeGroupDto {
     private Long ageGroupId;
     private String shortCode;
--- active-life-canada-app/src/main/java/io/reactivestax/active/life/canada/dto/FacilityDto.java
+++ active-life-canada-app/src/main/java/io/reactivestax/active/life/canada/dto/FacilityDto.java
@@ -1,4 +1,22 @@
 package io.reactivestax.active.life.canada.dto;
 
-public class FacilityDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FacilityDto extends NameDescription {
+    private Long facilityId;
+    private String streetNo;
+    private String streetName;
+    private String city;
+    private String province;
+    private String country;
+    private String postalCode;
 }
