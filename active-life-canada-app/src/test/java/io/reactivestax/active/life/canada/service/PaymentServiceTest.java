1755271ed361665fe84bff7ff0d25f6d6d01e7c2
Anant.Jain <anant.jain@fullstacklabs.digital>
2025-02-10 15:58:30 -0500

    updated api contract

--- active-life-canada-app/src/test/java/io/reactivestax/active/life/canada/service/PaymentServiceTest.java
+++ active-life-canada-app/src/test/java/io/reactivestax/active/life/canada/service/PaymentServiceTest.java
@@ -1,5 +1,7 @@
 package io.reactivestax.active.life.canada.service;
 
+import io.reactivestax.active.life.canada.exception.InvalidRequestException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,9 +9,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -31,12 +35,13 @@ class PaymentServiceTest {
         assertDoesNotThrow(() -> paymentService.createPaymentIntentAndConfirm(200, ""));
     }
 
-//    @Test
-//    void testCreatePaymentIntentAndConfirm_Failed() {
-//        when(restTemplate.postForEntity(any(), any(HttpEntity.class), eq(String.class)))
-//                .thenReturn(ResponseEntity.badRequest().build());
-//
-//        assertThrows(InvalidRequestException.class,
-//                () -> paymentService.createPaymentIntentAndConfirm(200, ""));
-//    }
+    @Disabled("This test is ignored for now as it is failing because of failing stubbing")
+    @Test
+    void testCreatePaymentIntentAndConfirm_Failed() {
+        when(restTemplate.postForEntity(any(), any(HttpEntity.class), eq(String.class)))
+                .thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
+
+        assertThrows(InvalidRequestException.class,
+                () -> paymentService.createPaymentIntentAndConfirm(200, ""));
+    }
 }
