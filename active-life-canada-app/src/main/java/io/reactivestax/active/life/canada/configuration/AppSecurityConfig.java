726ab0470de96be627f82cd6c7fa87e00c94a415
Anant.Jain <anant.jain@fullstacklabs.digital>
2025-02-19 16:40:38 -0500

    added spring security

---
+++ active-life-canada-app/src/main/java/io/reactivestax/active/life/canada/configuration/AppSecurityConfig.java
@@ -0,0 +1,20 @@
+package io.reactivestax.active.life.canada.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+
+        return http.build();
+    }
+}
